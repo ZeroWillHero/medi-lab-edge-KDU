e66f96938c0e6eaeff04e1d123d2d5dd592cefb3
Chameera Sandakelum <127183569+ZeroWillHero@users.noreply.github.com>
2024-05-12 01:08:30 +0530

    new updated

--- src/components/ClientExp.jsx
+++ src/components/ClientExp.jsx
@@ -2,13 +2,13 @@ export default function ClientExp () {
     return (
         <form className="grid grid-rows-7 gap-1 items-center">
             <label className="text-cyan-500 font-bold text-xl" htmlFor="name">name</label>
-            <input className="h-10 ps-2 " type="text" placeholder="Name" />
+            <input className="h-10 ps-2 text-black " type="text" placeholder="Name" />
 
             <label className="text-cyan-500 font-bold text-xl" htmlFor="position">position</label>
-            <input className="h-10 ps-2 " type="text" placeholder="position" />
+            <input className="h-10 ps-2 text-black " type="text" placeholder="position" />
 
             <label className="text-cyan-500 font-bold text-xl" htmlFor="discription">Discription</label>
-            <input className="h-20 w-auto ps-2" type="text" placeholder="discription" />
+            <input className="h-20 w-auto ps-2 text-black" type="text" placeholder="discription" />
 
             <button className="bg-cyan-500 hover:bg-cyan-400 p-3 rounded-md">Upload</button>
         </form>
--- src/components/NewsUpload.jsx
+++ src/components/NewsUpload.jsx
@@ -2,10 +2,10 @@ export default function NewsUpload() {
     return (
         <form className="grid grid-rows-5 gap-1 items-center">
             <label className="text-cyan-500 font-bold text-xl" htmlFor="headline">Headline</label>
-            <input className="h-10 ps-2 " type="text" placeholder="Enter your headline" />
+            <input className="h-10 ps-2 text-black " type="text" placeholder="Enter your headline" />
 
             <label className="text-cyan-500 font-bold text-xl" htmlFor="discription">Discription</label>
-            <input className="h-20 w-auto ps-2" type="text" placeholder="discription" />
+            <input className="h-20 w-auto ps-2 text-black" type="text" placeholder="discription" />
 
             <button className="bg-cyan-500 hover:bg-cyan-400 p-3 rounded-md">Upload</button>
         </form>
